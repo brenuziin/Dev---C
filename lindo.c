7446b95fd460f9c2abf58f122fdd0833591b4439
brenuziin <161034670+brenuziin@users.noreply.github.com>
2024-03-15 09:29:33 -0300

    Add files via upload

---
+++ lindo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+	
+	printf("Se voce recebeu essa mensagem, voce foi SELECIONADO, mas secretamente.");
+	printf("\nNao deixe essa mensagem cair em publico, nosso planeta esta em perigo.");
+	printf("\nVoce nao esta seguro, nos precisamos de sua ajuda, voce pode salvar nosso planeta."); 
+	
+	getchar();
+	
+    return 0;
+}
