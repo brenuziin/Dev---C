a10a6db488d390106f88b166990a02dcc6fd0338
brenuziin <161034670+brenuziin@users.noreply.github.com>
2024-03-19 08:24:37 -0300

    Add files via upload

---
+++ coracao.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main () {
+	
+	    setlocale(LC_ALL, ""); //Indentificar o Idioma	
+	   
+	// Declarando Variáveis
+	char nome[200];
+	int idade, quanto;
+	float notaUm, notaDois, notaTres, notaQuatro, media, resultado;
+	
+	// Solicitando dados para o Usuário
+	
+	printf("\nquantos porcento vc me ama: ");
+	scanf("%i", &quanto);
+	
+	fflush(stdin);
+printf ("MEU AMOR POR VC NAO PODE SER MEDIDO");
+printf("\n ______00000000000_______00000000000");
+printf("\n_____000000000000000__0000000000000000");
+printf("\n___0000000000000000000000000_______00000");
+printf("\n__00000000000000000000000000_________0000");
+printf("\n_00000000000000000000000000000________0000");
+printf("\n0000000000000000000000000000000000_____0000");
+printf("\n00000000000000000000000000000000000___00000");
+printf("\n000000000000000000000000000000000000_000000");
+printf("\n_00000000000000000000000000000000000000000");
+printf("\n__000000000000000000000000000000000000000");
+printf("\n____00000000000000000000000000000000000");
+printf("\n______0000000000000000000000000000000");
+printf("\n_________00000000000000000000000000");
+printf("\n____________000000000000000000000");
+printf("\n______________0000000000000000");
+printf("\n_________________0000000000");
+printf("\n___________________000000");
+printf("\n_____________________000");
+
+return 0;
+
+}
+
